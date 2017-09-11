714650e5191857f3eb8d1374c9ff3d66ff343858
Mark Lindamood <mark.lindamood@yahoo.com>
2017-09-11 15:18:53 -0700

    created struct for Player; disabled "Next" button; created IBActions for "Mens", "Womens" and "Coed" button;  based on selected league, re-enabled "Next" button

--- app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/Controller/LeagueVC.swift
@@ -9,17 +9,34 @@
 import UIKit
 
 class LeagueVC: UIViewController {
-   
+    @IBOutlet weak var nextBtn: BorderButton!
+    
+    var player: Player!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        player = Player()
     }
 
     
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
+    }
+    
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectedLeague(leagueType: "mens")
+    }
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectedLeague(leagueType: "womens")
+    }
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectedLeague(leagueType: "coed")
+    }
+    
+    func selectedLeague(leagueType: String) {
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
     }
     
     @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
